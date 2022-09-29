ba293f51fea97f328664498e986483203844d725
PleasePomogite <114631827+PleasePomogite@users.noreply.github.com>
2022-09-29 08:55:06 +0700

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,11 @@
+a="абвгдеёжзийк"
+alf=list(a)
+print (alf)
+mors=".- -... .-- --. -.. . .... ...- --.. .. .--- -.-"
+alfm=mors.split()
+print (alfm)
+morze=input("текст")
+for i in range(len(morze)):
+    print (morze[i])
+    
+
