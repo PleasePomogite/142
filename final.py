eb649dd37c50f36302e7702d4c84afbe329522d5
PleasePomogite <114631827+PleasePomogite@users.noreply.github.com>
2022-11-24 10:11:39 +0700

    Add files via upload
    не работает кста

---
+++ final.py
@@ -0,0 +1,36 @@
+from moviepy.editor import *
+from PIL import Image, ImageDraw, ImageFont
+import os
+texts = [
+    'Yare yare'
+    'Я люблю рогалики'
+    'Прослушайте текст и напишите сжатое изложение'
+]
+
+def f(name,text):
+    name = 'C:/Users/user/Desktop/aaa/'+name + '.jpg'
+    im = Image.new('RGB', (500,500), color=('#FAACAC'))
+    draw_text = ImageDraw.Draw(im)
+    font = ImageFont.truetype("arial.ttf", 20, encoding='UTF-8')
+    draw_text.text(
+        (20,100),
+        text,
+        fill=('#1C0606'),
+        font= font
+    )
+    im.save(name)
+def f1():
+    direc = 'C:/Users/user/Desktop/aaa'
+    files = os.listdir(direc)
+    imag = list(filter(lambda x: x.endswith('.jpg'), files))
+    clips = [ImageClip(m).set_duration(2) for m in imag]
+
+    final = concatenate_videoclip(clips, method='compose')
+    final.write_videofile('test.mp4', fps=24)
+
+for i in range(len(texts)):
+    f(str(i),texts[i])
+    
+f1()
+    
+    
